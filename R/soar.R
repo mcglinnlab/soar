7d2929617a90a3d2b45f42bb58257014b231720d
Ashley Woods <woodsae@g.cofc.edu>
2018-03-03 09:57:04 -0500

    ignore this

--- R/soar.R
+++ R/soar.R
@@ -2,65 +2,80 @@ library(shiny)
 library(ggplot2)
 library(rinat)
 library(DT)
+library(leaflet)
 
 # create user interface
 ui <- fluidPage(
   
-    # Application title
-    titlePanel("SOAR: species occupancy aggregator in R"),
+  # Application title
+  titlePanel("SOAR: species occupancy aggregator in R"),
   
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-       sidebarPanel(
-          textInput("species_name", "Species name", "Bald eagle"),
-          checkboxInput("checkbox", label = "Input Latitude and Longitude bounds", value = FALSE),
-          conditionalPanel (
-            condition = "input.checkbox == true",
-            textInput("Lat_low", "Latitude lower range", "90"),
-            textInput("Lat_high", "Latitude upper range", "-90"),
-            textInput("Long_low", "Longitude lower range", "180"),
-            textInput("Long_high", "Longitude upper range", "-180")
-          ),
-          actionButton("do", "Submit")
-       ),
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      textInput("species_name", "Species name", "Bald eagle"),
+      checkboxInput("checkbox", label = "Input Latitude and Longitude bounds", value = FALSE),
+      conditionalPanel (
+        condition = "input.checkbox == true",
+        textInput("Lat_low", "Latitude lower range", "90"),
+        textInput("Lat_high", "Latitude upper range", "-90"),
+        textInput("Long_low", "Longitude lower range", "180"),
+        textInput("Long_high", "Longitude upper range", "-180")
+      ),
+      actionButton("do", "Submit")
+    ),
     
-       # Show a plot of the generated distribution
-       mainPanel(
-         h3(textOutput("species_name")),
-         tabsetPanel(
-           tabPanel("Map", plotOutput("world_map")),
-           tabPanel("Raw Data", DT::dataTableOutput("raw_data")),
-           tabPanel("SOAR Derived Fields")
-         )
-           #plotOutput("world_map")
-           #plotOutput("state_map")
-           #dataTableOutput("data")
-       )
+    # Show a plot of the generated distribution
+    mainPanel(
+      #h3(textOutput("species_name")),
+      tabsetPanel(
+        tabPanel("Map", leafletOutput("world_map")),
+        tabPanel("Raw Data", DT::dataTableOutput("raw_data")),
+        tabPanel("SOAR Derived Fields")
+      )
     )
+  )
 )
 
 # create server which is the logic of the app
 server <- function(input, output) {
-    observeEvent(input$do, {
-
-        output$species_name <- renderText({input$species_name})
-        
-        if (input$checkbox == TRUE) {
-        bounds <- c(input$Lat_high, input$Long_high, input$Lat_low, input$Long_low)
-        } else {bounds <- NULL}
-        
-        INATdata <- get_inat_obs(taxon_name = input$species_name, bounds = bounds)
-        output$raw_data <- DT::renderDataTable({expr = INATdata})
-                    
-        output$world_map <- renderPlot({
-            data <- get_inat_obs(taxon_name = input$species_name, bounds = bounds)
-            #data_map <- map(database ="world", xlim=c(input$Long_high, input$Long_low), ylim=c(input$Lat_high, input$Lat_low))
-            #points(data)
-            data_map <- inat_map(data, map = "world", plot = FALSE)
-            data_map + borders("state") + theme_bw()
-           # data_map + xlim=c(input$Long_high, input$Long_low) + ylim=c(input$Lat_high, input$Lat_low)
-        })
-    })
+  
+  # this conditional is causing problems with the submit button
+  # not sure why right now
+  #    if (input$checkbox == TRUE) {
+  #        bounds <- c(input$Lat_high, input$Long_high,
+  #                    input$Lat_low, input$Long_low)
+  #    } else bounds <- NULL
+  
+  inat_data <- eventReactive(input$do, {
+    
+    #To Do: provide user option for maxresults argument
+    inat_result <- get_inat_obs(taxon_name = input$species_name)
+    coords_na <- apply(inat_result[ , c('longitude', 'latitude')], 1,
+                       function(x) any(is.na(x)))
+    inat_result[!coords_na, ]
+  })
+  output$raw_data <- DT::renderDataTable(expr = inat_data())
+  
+  
+  #fileData <- write.csv(INATdata, file = "INATdata.csv", row.names = FALSE)
+  
+  pal <- colorFactor(palette = c("red", "orange", "navy"),
+                     domain = c("casual", "needs_id", "research"))
+  
+  output$world_map <- renderLeaflet({
+    leaflet(inat_data()) %>%
+      addProviderTiles(providers$Esri.NatGeoWorldMap) %>% 
+      addCircleMarkers(lng = ~ longitude, lat = ~latitude ,
+                       radius = ~ifelse(quality_grade == "research", 6, 3),
+                       color = ~pal(quality_grade),
+                       stroke = FALSE, fillOpacity = 0.5
+      ) %>%
+      addLegend("bottomright", pal = pal, values = ~quality_grade,
+                labels = c('casual', 'needs id', 'research'),
+                title = "The quality of the records"
+      )
+  })
 }
 
 # Create a Shiny app object
@@ -87,9 +102,8 @@ server <- function(input, output) {
 #' @export
 gui <- function(port = getOption("shiny.port"),
                 host = getOption("shiny.host", "127.0.0.1")) {
-
+  
   shiny::runApp(shinyApp(ui = ui, server = server),
                 display.mode = "normal", port = port, host = host)
   rm(ui, server, envir = .GlobalEnv)
 }
-
