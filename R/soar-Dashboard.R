b74f4fb1817adb9e34ea334b00b9338000a6f9aa
Woodsae <Woodsae@g.cofc.edu>
2018-06-29 08:53:22 -0400

    Changed all camelCase variables and functions to snake_case

--- R/soar-Dashboard.R
+++ R/soar-Dashboard.R
@@ -5,7 +5,7 @@ library(leaflet)
 library(rgbif)
 library(shinycssloaders)
 
-minChoice <- function() {
+min_choice <- function() {
   Gbif_fields <- read.csv("gbif_fields.csv", as.is = TRUE)
   # [row,col] cols: colName, MetaData, Minimal, Default, Custom
   lis<-c()
@@ -16,15 +16,15 @@ minChoice <- function() {
       lis2 <- c(lis2,i)
     }
   }
-  finalList <- list()
+  final_list <- list()
   for (i in 1:length(lis)) {
-    finalList[[lis[i]]] <- as.double(lis2 [i])
+    final_list[[lis[i]]] <- as.double(lis2 [i])
   }
   
-  return(finalList)
+  return(final_list)
   
 }
-defChoice <- function() {
+def_choice <- function() {
   Gbif_fields <- read.csv("gbif_fields.csv", as.is = TRUE)
   # [row,col] cols: colName, MetaData, Minimal, Default, Custom
   lis<-c()
@@ -35,15 +35,15 @@ defChoice <- function() {
       lis2 <- c(lis2,i)
     }
   }
-  finalList <- list()
+  final_list <- list()
   for (i in 1:length(lis)) {
-    finalList[[lis[i]]] <- as.double(lis2 [i])
+    final_list[[lis[i]]] <- as.double(lis2 [i])
   }
   
-  return(finalList)
+  return(final_list)
   
 }
-cusChoice <- function() {
+cus_choice <- function() {
   Gbif_fields <- read.csv("gbif_fields.csv", as.is = TRUE)
   # [row,col] cols: colName, MetaData, Minimal, Default, Custom
   lis<-c()
@@ -54,12 +54,12 @@ cusChoice <- function() {
       lis2 <- c(lis2,i)
     }
   }
-  finalList <- list()
+  final_list <- list()
   for (i in 1:length(lis)) {
-    finalList[[lis[i]]] <- as.double(lis2 [i])
+    final_list[[lis[i]]] <- as.double(lis2 [i])
   }
   
-  return(finalList)
+  return(final_list)
   
 }
 #create a user interface
@@ -79,10 +79,10 @@ ui <- dashboardPage(
     textInput("gbif_email", "Gbif Email", "Ex: JaneSmith@generic.com"),
     
     #input latlong bounding box
-    checkboxInput("latLongcheckbox", label = "Input Geographical Boundaries with Latitude and Longitude", 
+    checkboxInput("lat_long_check_box", label = "Input Geographical Boundaries with Latitude and Longitude", 
                   value = FALSE),
     conditionalPanel (
-      condition = "input.latLongcheckbox == true",
+      condition = "input.lat_long_check_box == true",
       textInput("Lat_low", "Latitude lower range", "-90"),
       textInput("Lat_high", "Latitude upper range", "90"),
       textInput("Long_low", "Longitude lower range", "-180"),
@@ -90,24 +90,24 @@ ui <- dashboardPage(
     ),
     
     #input bounding box with political boundaries
-    checkboxInput("politicalcheckbox", label = "Input Geographical boundaries by country"),
+    checkboxInput("political_checkbox", label = "Input Geographical boundaries by country"),
     conditionalPanel(
-      condition ="input.politicalcheckbox == true",
+      condition ="input.political_checkbox == true",
       textInput("political_boundary", "Country Name", "Country Code")
       ),
    
      #input date bounding information for gbif
-    checkboxInput("datecheckbox", label = "Sort by date", value = FALSE),
+    checkboxInput("date_checkbox", label = "Sort by date", value = FALSE),
     conditionalPanel (
-      condition = "input.datecheckbox == true",
+      condition = "input.date_checkbox == true",
       textInput("from_date", "Show results from this month:", "mm/yyyy"),
       textInput("to_date", "to this month:", "mm/yyyy")
     ),
   
     #input specific download key
-    checkboxInput("downKey", label = "Specify Download Key", value = FALSE),
+    checkboxInput("down_key_checkbox", label = "Specify Download Key", value = FALSE),
     conditionalPanel (
-      condition = "input.downKey == true",
+      condition = "input.down_key_checkbox == true",
       textInput("down_key", "Download Key:", "" )
     ),
       
@@ -120,30 +120,30 @@ ui <- dashboardPage(
       tabPanel("Map", withSpinner(leafletOutput("world_map")),
 #Add downsampling options here
                box(title = "Down-sample Tools", "Option 1:",
-                actionButton("DefDown", "Down-Sample data using simple random sampling"), br(),
+                actionButton("def_down", "Down-Sample data using simple random sampling"), br(),
                 "Option 2:", 
-                textInput("GridCellSize", "Grid Cell Size: "), textInput("MinCellDen", "Minimum Cell Density:"), 
-                actionButton("CusDown", "Down-Sample data with custom settings"))),
+                textInput("grid_cell_size", "Grid Cell Size: "), textInput("MinCellDen", "Minimum Cell Density:"), 
+                actionButton("cus_down", "Down-Sample data with custom settings"))),
       tabPanel("Raw Data", DT::dataTableOutput("raw_data")),
       tabPanel("Download table",
-               radioButtons("tableCols", label = "Columns in Downloadable table",
+               radioButtons("table_cols", label = "Columns in Downloadable table",
                                   choices = list("Minimal" = 1, "Default" = 2, "All Columns" = 3, "Custom" = 4), 
                                   selected = 1),
 #ADD - Download link for table
-               downloadButton('downloadData', label = "Download Table"),
-               conditionalPanel(condition = "input.tableCols == 1", 
-                                checkboxGroupInput("tableMin", label = h4("Minimal Options:"), choices =
-                                                   minChoice(), 
+               downloadButton('download_data', label = "Download Table"),
+               conditionalPanel(condition = "input.table_cols == 1", 
+                                checkboxGroupInput("table_min", label = h4("Minimal Options:"), choices =
+                                                   min_choice(), 
                                                    selected = c(133, 134, 175, 220, 229))),
-               conditionalPanel(condition = "input.tableCols == 2",
-                                checkboxGroupInput("tableDef", label= h4("Default Options:"), choices = defChoice(),
+               conditionalPanel(condition = "input.table_cols == 2",
+                                checkboxGroupInput("table_def", label= h4("Default Options:"), choices = def_choice(),
                                                    selected = c(43,47,48,60,65,69,75,76,106,121,133:135,175,183,191:200,207,219,229,230))
                                 ),
-               conditionalPanel(condition = "input.tableCols == 3",
+               conditionalPanel(condition = "input.table_cols == 3",
                                 h4("Table will contain all columns.")),
-               conditionalPanel(condition = "input.tableCols == 4",
+               conditionalPanel(condition = "input.table_cols == 4",
                                 #checkboxes with 235 options
-                                checkboxGroupInput("tableCus", label= h4("Select Options (Defaults Options Selected):"), choices = cusChoice(),
+                                checkboxGroupInput("table_cus", label= h4("Select Options (Defaults Options Selected):"), choices = cus_choice(),
                               selected = c(43,47,48,60,65,69,75,76,106,121,133:135,175,183,191:200,207,219,229,230))
                                 )
                )
@@ -159,82 +159,82 @@ server <- function(input, output) {
     #filter by name
     sp_key <- name_suggest(q =input$species_name, rank = input$rank)$key[1]
     #filter by country
-    if (input$politicalcheckbox){ country <- input$political_boundary}
+    if (input$political_checkbox){ country <- input$political_boundary}
     #filter by date
-    if (input$datecheckbox){
-      toM <- substr(input$to_date, 1,2)
-      fromM<- substr(input$from_date, 1,2)
-      toY <- substr(input$to_date, 4, 7)
-      fromY <- substr(input$from_date, 4, 7)
+    if (input$date_checkbox){
+      to_m <- substr(input$to_date, 1,2)
+      from_m<- substr(input$from_date, 1,2)
+      to_y <- substr(input$to_date, 4, 7)
+      from_y <- substr(input$from_date, 4, 7)
     }
     #filter by lat/long
-    if (input$latLongcheckbox){
-      latLow <- input$Lat_low
-      latHigh <- input$Lat_high
-      longLow <- input$Long_low
-      longHigh <- input$Long_high
+    if (input$lat_long_check_box){
+      low_lat <- input$Lat_low
+      high_lat <- input$Lat_high
+      low_long <- input$Long_low
+      high_long <- input$Long_high
     }
 #empty fields cannot be set to NULL, this sees what data is added and runs the correct
 #request depending on what fields are full. It's really big
-    if (input$downKey){
+    if (input$down_key_checkbox){
       continue = FALSE
       dat <- occ_download_get(key = toString(input$down_key), overwrite = TRUE) %>% occ_download_import()
       return(dat)
     }
-    else if (input$latLongcheckbox){
+    else if (input$lat_long_check_box){
       continue = TRUE
-      if (input$datecheckbox & input$politicalcheckbox){
-        res = occ_download(paste("decimalLatitude >=", latLow), paste("decimalLatitude <=", latHigh),
-                           paste("decimalLongitude >=", longLow), paste("decimalLongitude <=", longHigh),
-                           paste("year >=", fromY), paste("year <=", toY), paste("month >=", fromM),
-                           paste("month <=", toM), paste("country =", country), 
+      if (input$date_checkbox & input$political_checkbox){
+        res = occ_download(paste("decimalLatitude >=", low_lat), paste("decimalLatitude <=", high_lat),
+                           paste("decimalLongitude >=", low_long), paste("decimalLongitude <=", high_long),
+                           paste("year >=", from_y), paste("year <=", to_y), paste("month >=", from_m),
+                           paste("month <=", to_m), paste("country =", country), 
                            paste("taxonKey =", sp_key), 'hasCoordinate = TRUE',
                            user = input$gbif_username, pwd = input$gbif_pass, 
                            email = input$gbif_email)
       }
-      else if (input$datecheckbox){
-        res = occ_download(paste("decimalLatitude >=", latLow), paste("decimalLatitude <=", latHigh),
-                           paste("decimalLongitude >=", longLow), paste("decimalLongitude <=", longHigh),
-                           paste("year >=", fromY), paste("year <=", toY), paste("month >=", fromM),
-                           paste("month <=", toM), 
+      else if (input$date_checkbox){
+        res = occ_download(paste("decimalLatitude >=", low_lat), paste("decimalLatitude <=", high_lat),
+                           paste("decimalLongitude >=", low_long), paste("decimalLongitude <=", high_long),
+                           paste("year >=", from_y), paste("year <=", to_y), paste("month >=", from_m),
+                           paste("month <=", to_m), 
                            paste("taxonKey =", sp_key), 'hasCoordinate = TRUE',
                            user = input$gbif_username, pwd = input$gbif_pass, 
                            email = input$gbif_email)
       }
-      else if (input$politicalcheckbox){
-        res = occ_download(paste("decimalLatitude >=", latLow), paste("decimalLatitude <=", latHigh),
-                           paste("decimalLongitude >=", longLow), paste("decimalLongitude <=", longHigh),
+      else if (input$political_checkbox){
+        res = occ_download(paste("decimalLatitude >=", low_lat), paste("decimalLatitude <=", high_lat),
+                           paste("decimalLongitude >=", low_long), paste("decimalLongitude <=", high_long),
                            paste("country =", country), 
                            paste("taxonKey =", sp_key), 'hasCoordinate = TRUE',
                            user = input$gbif_username, pwd = input$gbif_pass, 
                            email = input$gbif_email)
       }
       else{
-        res = occ_download(paste("decimalLatitude >=", latLow), paste("decimalLatitude <=", latHigh),
-                           paste("decimalLongitude >=", longLow), paste("decimalLongitude <=", longHigh),
+        res = occ_download(paste("decimalLatitude >=", low_lat), paste("decimalLatitude <=", high_lat),
+                           paste("decimalLongitude >=", low_long), paste("decimalLongitude <=", high_long),
                            paste("taxonKey =", sp_key), 'hasCoordinate = TRUE',
                            user = input$gbif_username, pwd = input$gbif_pass, 
                            email = input$gbif_email)
       }
     }
-    else if (input$datecheckbox){
+    else if (input$date_checkbox){
       continue = TRUE
-      if (input$politicalcheckbox){
-        res = occ_download(paste("year >=", fromY), paste("year <=", toY), paste("month >=", fromM),
-                           paste("month <=", toM), paste("country =", country), 
+      if (input$political_checkbox){
+        res = occ_download(paste("year >=", from_y), paste("year <=", to_y), paste("month >=", from_m),
+                           paste("month <=", to_m), paste("country =", country), 
                            paste("taxonKey =", sp_key), 'hasCoordinate = TRUE',
                            user = input$gbif_username, pwd = input$gbif_pass, 
                            email = input$gbif_email)
       }
       else{
-        res = occ_download(paste("year >=", fromY), paste("year <=", toY), paste("month >=", fromM),
-                           paste("month <=", toM), 
+        res = occ_download(paste("year >=", from_y), paste("year <=", to_y), paste("month >=", from_m),
+                           paste("month <=", to_m), 
                            paste("taxonKey =", sp_key), 'hasCoordinate = TRUE',
                            user = input$gbif_username, pwd = input$gbif_pass, 
                            email = input$gbif_email)
       }
     }
-    else if (input$politicalcheckbox){
+    else if (input$political_checkbox){
       continue = TRUE
       res = occ_download(paste("country =", country), 
                          paste("taxonKey =", sp_key), 'hasCoordinate = TRUE',
@@ -268,31 +268,31 @@ server <- function(input, output) {
   })
   
 #Displays Entire Dataset
-  #-Changed gbif_data() to downloadInfo() to only show cols selected by user
-  output$raw_data <- DT::renderDataTable(expr = downloadInfo(),options=list(autoWidth = TRUE,scrollX=TRUE))
+  #-Changed gbif_data() to download_info() to only show cols selected by user
+  output$raw_data <- DT::renderDataTable(expr = download_info(),options=list(autoWidth = TRUE,scrollX=TRUE))
   
 #Default Cols? [,c(43,47,48,60,65,69,75,76,106,121,133:135,175,183,191:200,207,219,229,230)]
 #Makes A Downloadable Table for only the columns selected
-  downloadInfo <- function(){
+  download_info <- function(){
     gDat <- gbif_data()
-    if(input$tableCols == 1){
-      return(gDat[ , as.numeric(input$tableMin)])
+    if(input$table_cols == 1){
+      return(gDat[ , as.numeric(input$table_min)])
     }
-    else if(input$tableCols == 2){
-      return(gDat[ , as.numeric(input$tableDef)])
+    else if(input$table_cols == 2){
+      return(gDat[ , as.numeric(input$table_def)])
     }
-    else if(input$tableCols == 3){
+    else if(input$table_cols == 3){
       return(gDat)
     }
     else{
-      return(gDat[ , as.numeric(input$tableCus)])
+      return(gDat[ , as.numeric(input$table_cus)])
     }
   }
   
-  output$downloadData <- downloadHandler(
+  output$download_data <- downloadHandler(
     filename = function(){"gbifDat.csv"},
     content = function(file){
-      write.csv(downloadInfo(), file)
+      write.csv(download_info(), file)
     }
   )
   
